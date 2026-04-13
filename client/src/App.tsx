fa1502d883baebfcda1108eae5dceab98845a778
realperformancesolutions-create <272331201+realperformancesolutions-create@users.noreply.github.com>
2026-04-12 21:40:40 -0400

    CFA: set root route to CFA page, update title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,12 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
 import CFA from "./pages/CFA";
 
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/cfa"} component={CFA} />
+      <Route path={"/"} component={CFA} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
     </Switch>
